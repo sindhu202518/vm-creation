d22f9c7e1a4412657e6dc4bebd84968a88a74af8
sindhu202518 <gunasekarandba@gmail.com>
2025-06-03 07:38:48 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,12 @@ provider "google" {
   project = "auto-project1-461104"
   region  = "asia-south1"
 }
-variable "vm_names" {
-  type    = list(string)
-  default = ["my-vm201", "my-vm202"]
-}
 
 resource "google_compute_instance" "vm_instance" {
-  for_each    = toset(var.vm_names)
-  name        = each.value
+  count        = 2
+  name         = "mygu-vm-${count.index + 1}"
   machine_type = "e2-medium"
-  zone        = "asia-south1-a"
+  zone         = "asia-south1-a"
 
   boot_disk {
     initialize_params {
